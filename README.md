0f9aed1b7d691957fd976eae87d5bfee5dfc74af
Andy Ma <andyjma.work@gmail.com>
2022-03-25 15:45:33 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+register.py is used to register for classes. 
+The input is a file containing the date and time of registration (first line)
+and the courses to be registered. (second line)
+
+The program primarily uses the library pynput to generate keyboard inputs. 
+Instructions:
+store the time in the first line in the input file in the following format:
+year month day hour minute
+* if there is no minute, enter 0 for minute
+
+enter the course numbers in order separated by spaces
+course1 course2 ... 
---
+++ register.py
@@ -0,0 +1,65 @@
+import pynput
+import sys
+import pause
+from datetime import datetime
+from selenium import webdriver
+import chromedriver_binary
+
+def getdata(file):
+    with open(file) as f:
+        lines = f.readlines()
+    time = lines[0].strip()
+    courses = lines[1].strip()
+    return time, courses
+
+def enter_courses(class_string):
+    keyboard = pynput.keyboard.Controller()
+
+    #navigate to the first input
+    keyboard.press(pynput.keyboard.Key.tab)
+    keyboard.release(pynput.keyboard.Key.tab)
+
+    for char in class_string:
+        if char == ' ':
+            keyboard.press(pynput.keyboard.Key.tab)
+            keyboard.release(pynput.keyboard.Key.tab)
+            continue
+        elif char == '!': 
+            keyboard.press(pynput.keyboard.Key.enter)
+            keyboard.release(pynput.keyboard.Key.enter)
+            continue
+        keyboard.press(char)
+        keyboard.release(char)
+    print("Courses entered")        
+
+if __name__ == '__main__':
+    if len(sys.argv)!=2:
+        print(f"Wrong number of input files {len(sys.argv)}")
+    filename = sys.argv[1];
+
+    #read data
+    timestr, coursestr = getdata(filename)
+    time = [int(x) for x in timestr.split(' ')]
+    time = datetime(time[0], time[1], time[2], time[3], time[4])
+
+    #refresh the browser
+    driver = webdriver.Chrome()
+    # executable_path=r"C:\Users\Andy\Downloads\miniconda\Lib\site-packages\chromedriver_binary.exe"
+    driver.get("https://sis.rpi.edu/rss/bwskfreg.P_AltPin")
+    # driver.get("https://google.com")
+
+    #pause until registration opens
+    pause.until(time)
+    print("done pausing")
+
+    driver.refresh();
+    #wait 2 seconds don't know a better way to do this lol
+    driver.implicitly_wait(2)
+    enter_courses(coursestr)
+
+
+    
+
+    
+    
+
